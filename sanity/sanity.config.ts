8afa13563b9eccf6eb59847c00ce95e8ae9794e2
akash0526 <akashadhikari0526@gmail.com>
2026-01-15 19:29:11 +0545

    somechanges to fix project id unavailavle in sanity

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,16 +1,12 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { schemaTypes } from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
   title: 'dailyarticles',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: 'production',
-
-  plugins: [structureTool()], // Make sure visionTool() is GONE from here
-
-  schema: {
-    types: schemaTypes,
-  },
+  projectId: process.env.SANITY_PROJECT_ID || 'x6apaaxw', // fallback hardcoded
+  dataset: process.env.SANITY_DATASET || 'production',
+  plugins: [structureTool()],
+  schema: { types: schemaTypes },
 })
